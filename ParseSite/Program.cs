11b9d98b7d0493ba8b99801b79e69171c1579520
dude_just_dude <dude.just.dude@gmai.com>
2015-03-07 19:02:59 +0200

    парсинг fin.ua

---
+++ ParseSite/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExchangeRates.Data;
+using HtmlAgilityPack;
+using ParseSite.Fin.ua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace ParseSite
+{
+    class Program
+    {
+        private const string SITE = @"http://finance.i.ua/";
+        private const string USD = @"usd/";
+        private const string EUR = @"eur/";
+        private const string RUB = @"rub/";
+
+        static void Main(string[] args)
+        {
+
+            var cours = CParser.GetKursesFromFinUA(SITE, USD);
+
+            Console.ReadLine();
+        }
+    }
+}
